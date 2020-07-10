316db268af0b19866ee3c7706b0d704033ead7ce
user <user@i7C-59>
2020-07-10 10:32:24 +0900

    MyController 추가

---
+++ Ex26_GitTest/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,27 @@
+package com.study.springboot;
+
+public class MyController {
+
+	//수정작업 -> 
+	   
+	//Git Test
+	   
+	// Git 장점
+	// 1. 소스 안전 관리
+	// 2. 히스토리 관리 가능 - 수정/추가/삭제/변경 기록이 다 남음
+	// 3. 팀 작업 용이
+	   
+	// clone : 복제 (서버 저장소를 로컬에 복제함)
+	// master : 최상위 소스노드
+	// breanch : 가치치기( 중간에
+	   
+	// 프로젝트소스 폴더
+	//	          |    ^ 
+	//   commit   v    | fetch
+	// 로컬(내pc) 저장소
+	//      |    ^
+	// push v    | pull
+	// 원격저장소(Github 서버) 
+	
+	
+}
